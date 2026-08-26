5ede32b159ca573bd289759541f69bf2eef97346
Pimpcats <126701748+Pimpcats@users.noreply.github.com>
2026-08-26 09:53:06 -0700

    Inject custom content store into app

--- StreamPrep-iOS/StreamPrep/StreamPrepApp.swift
+++ StreamPrep-iOS/StreamPrep/StreamPrepApp.swift
@@ -3,11 +3,13 @@ import SwiftUI
 @main
 struct StreamPrepApp: App {
     @StateObject private var session = SessionStore()
+    @StateObject private var customContent = CustomContentStore()
 
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environmentObject(session)
+                .environmentObject(customContent)
         }
     }
 }
